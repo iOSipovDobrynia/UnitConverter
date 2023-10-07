4610c35ebc646d2b0d32eeba602a259aae110f97
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-07 10:53:00 +0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Goodwasp on 07.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
